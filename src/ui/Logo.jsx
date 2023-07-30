24297bf1706a970781dcba41f6aa6e1d9cef60b5
Anik Paul <132483203+anikpaul99@users.noreply.github.com>
2023-07-30 09:21:50 +0600

    :sparkles: feat: add app layout

---
+++ src/ui/Logo.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const StyledLogo = styled.div`
+  text-align: center;
+`;
+
+const Img = styled.img`
+  height: 9.6rem;
+  width: auto;
+`;
+
+/**
+ * Logo of the app
+ * @returns {JSX.Element}
+ * @author Anik Paul
+ */
+function Logo() {
+  return (
+    <StyledLogo>
+      <Img src="/logo-light.png" alt="Logo" />
+    </StyledLogo>
+  );
+}
+
+export default Logo;
