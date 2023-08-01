922463470a7adaa2455204ede942c65a94fb9c71
Anik Paul <132483203+anikpaul99@users.noreply.github.com>
2023-08-01 09:57:01 +0600

    :sparkles: feat: add form to create new cabin

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
+
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 /**
  * Page about cabins to be displayed when visited to '/cabins' URL
@@ -8,6 +12,8 @@ import Row from "../ui/Row";
  * @author Anik Paul
  */
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
       <Row type="horizontal">
@@ -17,6 +23,11 @@ function Cabins() {
 
       <Row>
         <CabinTable />
+
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
