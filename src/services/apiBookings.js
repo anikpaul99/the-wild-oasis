fd9f63df543ac9886056c580c06e8dceff62c3ca
Anik Paul <132483203+anikpaul99@users.noreply.github.com>
2023-08-12 22:55:41 +0600

    :sparkles: feat: sort bookings table
    sort bookings by date and amount

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -5,10 +5,11 @@ import { getToday } from "../utils/helpers";
 /**
  * Will fetch data about "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice" from the bookings table, from the database. Will also fetch data about 'name' from the  'cabins' table and the data about 'fullName & email' from the 'guests' table, from the database.
  * @prop {Object} filter The object which contains both the 'field' and the 'value' from the URL on which the query will be based on to implement the 'filter' operation.
+ * @prop {Object} sortBy Contains the 'field' and 'direction' the table should be sorted by.
  * @returns {Object []} The bookings data along with cabins data (name) and guests data (fullName, email).
  * @author Anik Paul
  */
-export async function getBookings({ filter }) {
+export async function getBookings({ filter, sortBy }) {
   let query = supabase
     .from("bookings")
     .select(
@@ -16,8 +17,11 @@ export async function getBookings({ filter }) {
     );
 
   // FILTER
-  if (filter !== null)
-    query = query[filter.method || "eq"](filter.field, filter.value);
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+
+  // SORT
+  if (sortBy)
+    query.order(sortBy.field, { ascending: sortBy.direction === "asc" });
 
   const { data, error } = await query;
 
